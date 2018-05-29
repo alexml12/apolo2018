4c94023230a34397777f851ac7c9d988d785cde2
jhonny <jhonnydy185@gmail.com>
2018-05-29 17:57:43 -0500

    configuracion 2
    comimit 2 jhonny

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost:888/egresados';
+$config['base_url']	= 'http://localhost:80/apolo2018';
 
 /*
 |--------------------------------------------------------------------------
