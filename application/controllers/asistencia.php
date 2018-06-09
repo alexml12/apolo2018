b03522a62f107deaf59ccea8fee5334f5f703d8a
alexml12 <alexanderml_12@hotmail.com>
2018-06-09 09:50:13 -0500

    commit alex
    actualizacion del modelo asistencia

--- application/controllers/asistencia.php
+++ application/controllers/asistencia.php
@@ -18,6 +18,7 @@ class asistencia extends CI_Controller
 
  public function index(){
           /*Si el usuario esta logeado*/
+          $this->Seguridad();
           $this->load->view('header');    
           $data['alumnos'] = $this->model_asistencia->ListarComensales();     
           $this->load->view('view_asistencia_reporte',$data);
@@ -35,7 +36,7 @@ class asistencia extends CI_Controller
     if($this->form_validation->run() == TRUE){
         //Verificamos si existe el email
          $VerifyExist = $this->model_asistencia->ExisteEmail($this->input->post("codigo_alumno"));
-         $RegistroHoy = $this->model_asistencia->SeRegistroHoy($this->input->post("codigo_alumno"),$hoy);
+         $RegistroHoy = $this->model_asistencia->SeRegistroHoy($this->input->post("codigo_alumno"), $this->input->post("fecha_Registro"));
         
         if($RegistroHoy>0){
                   $this->session->set_flashdata('msg', '<div class="alert alert-error text-center">El usuario ya se registro hoy </div>');
@@ -46,7 +47,7 @@ class asistencia extends CI_Controller
 
                     if($VerifyExist>0){
                     $UsuariosInsertar = $this->input->post();//Recibimos todo los campos por array nos lo envia codeigther
-                    $UsuariosInsertar["fecha_Registro"] = $hoy;//le agregamos la fecha de registro
+                    //$UsuariosInsertar["fecha_Registro"] = $hoy;//le agregamos la fecha de registro
                     //guardamos los registros
                     $this->model_asistencia->save($UsuariosInsertar);
                     redirect("asistencia?save=true"); 
@@ -61,7 +62,7 @@ class asistencia extends CI_Controller
                      else {
 
                     $UsuariosInsertar = $this->input->post();//Recibimos todo los campos por array nos lo envia codeigther
-                    $UsuariosInsertar["fecha_Registro"] = $hoy;//le agregamos la fecha de registro
+                    //$UsuariosInsertar["fecha_Registro"] = $hoy;//le agregamos la fecha de registro
                     //guardamos los registros
                     $this->model_asistencia->save($UsuariosInsertar);
                     redirect("asistencia?save=true");
@@ -77,11 +78,86 @@ class asistencia extends CI_Controller
     }
      }
 
+
+
       function ValidaCampos(){
     /*Campos para validar que no esten vacio los campos*/
      $this->form_validation->set_rules("codigo_alumno", "codigo_alumno", "trim|required|valid_codigo_alumno");
 
     }
+    public function editar($id = NULL){
+    
+    if ($id == NULL ){
+      $data['Modulo']  = "Asistencia";
+      $data['Error']   = "Error: El ID <strong>".$id."</strong> No es Valido, Verifica tu Busqueda !!!!!!!";
+      $this->load->view('header');
+      $this->load->view('view_errors',$data);
+      $this->load->view('footer');
+      return;
+    }
+    if ($this->input->post()) {
+      
+      $this->ValidaCampos();
+        
+      if ($this->form_validation->run() == TRUE){
+        $datos_update = $this->input->post();
+        $id_insertado = $this->model_asistencia->edit($datos_update,$id);
+        redirect('asistencia?update=true');
+        
+      }else{
+        $this->Nuevo();
+      }
+      
+    }else{
+      $data['datos_usuarios'] = $this->model_asistencia->BuscarID($id);
+      if (empty($data['datos_usuarios'])){
+        $data['Modulo']  = "Asistencia";
+        $data['Error']   = "Error: El ID <strong>".$id."</strong> No es Valido, Verifica tu Busqueda !!!!!!!";
+        $this->load->view('header');
+        $this->load->view('view_errors',$data);
+        $this->load->view('footer');
+      }else{
+        $this->load->view('header');
+        $this->load->view('view_edit_asistencia',$data);
+        $this->load->view('footer');
+      }
+    }
+    
+  }
+
+    public function eliminar($id = NULL){
+    if ($id == NULL ){
+      $data['Modulo']  = "Asistencia";
+      $data['Error']   = "Error: El ID <strong>".$id."</strong> No es Valido, Verifica tu Busqueda !!!!!!!";
+      $this->load->view('header');
+      $this->load->view('view_errors',$data);
+      $this->load->view('footer');
+      return;
+    }
+    if ($this->input->post()) {
+      $id_eliminar = $this->input->post('id_asistencia');
+      $boton       = strtoupper($this->input->post('btn_guardar'));
+      if($boton=="NO"){
+        redirect("Asistencia");
+      }else{
+                                $this->model_asistencia->Eliminar($id_eliminar);
+        redirect("asistencia?delete=true");
+      }
+    }else{
+      $data['datos_usuarios'] = $this->model_asistencia->BuscarID($id);
+      if (empty($data['datos_usuarios'])){
+        $data['Modulo']  = "Asistencia";
+        $data['Error']   = "Error: El ID <strong>".$id."</strong> No es Valido, Verifica tu Busqueda !!!!!!!";
+        $this->load->view('header');
+        $this->load->view('view_errors',$data);
+        $this->load->view('footer');
+      }else{
+        $this->load->view('header');
+        $this->load->view('view_delete_asistencia',$data);
+        $this->load->view('footer');
+      }
+    }
+  }
 
 
 }
